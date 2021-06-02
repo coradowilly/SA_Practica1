0ce6d83bea3aea87dee6d57e5cf61257db2e0fd2
coradowilly <coradowilly@gmail.com>
2021-06-02 11:47:12 -0600

    pagina web & pruebas unitarias

---
+++ client/src/app/app.component.spec.ts
@@ -0,0 +1,59 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'client'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('client');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content h1').textContent).toContain('Inicia sesión');
+  });
+
+  describe('Test for Login -> iniciarSesion()',() =>{
+    it('Shoul change user and password var',()=>{
+      component.iniciarSesion();
+      expect(component.user.email).not.toEqual("");
+      expect(component.user.password).not.toEqual("");
+    });
+
+    it('Shoul email and password is correct',()=>{
+      const r = component.iniciarSesion();
+      expect(r).toBeTrue();
+    })
+  });
+
+});
---
+++ client/src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'client';
+
+  user: any = {
+    email: 'coradowilly@gmail.com',
+    password: '1234',
+  };
+
+  usuarioactivo:any;
+  
+  iniciarSesion():Boolean{ 
+    console.log(this.user);
+    if(this.user.email == 'coradowilly@gmail.com' && this.user.password == '1234'){
+      this.showSuccess("Login correcto")
+      return true
+    } else {
+      this.showError("Login Incorrecto")
+      return false
+    }
+  }
+
+  showError(msj:string) {
+    //this.toastr.error(msj);
+    alert(msj);
+  }
+
+  showSuccess(msj: string) {
+    //this.toastr.success(msj);
+    alert(msj);
+  }
+
+  gotoRecuperarPassword() {
+    alert("Se te envio un correo con la recuperacion")
+  }
+}
