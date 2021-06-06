3202658167d2b9937b11dfc7e1f474446dd027e5
William <coradowilly@gmail.com>
2021-06-06 12:16:49 -0700

    test pipeline

--- karma.conf.js
+++ karma.conf.js
@@ -8,7 +8,6 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-firefox-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -25,9 +24,9 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: false,
-    browsers: ['Firefox'],
+    autoWatch: true,
+    browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: false
+    restartOnFileChange: true
   });
 };
